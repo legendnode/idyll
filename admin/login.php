a62afdef3c1bb6357fdd8f55165b52430cffa57e
Noah Zhang <226582@qq.com>
2022-11-08 14:04:04 +0800

    更新

--- admin/login.php
+++ admin/login.php
@@ -1,21 +1,25 @@
 <?php require __DIR__ . '/modules/header.php'; ?>
-<div class="login container">
-    <div class="board">
-        <h1 class="login-title"><?= _t('Idyll') ?></h1>
-        <form action="/user/login" method="POST">
-            <div class="field">
-                <input class="input" name="account" type="text" placeholder="<?= _t('用户名或邮箱') ?>" autofocus>
+<div class="login">
+    <div class="board card mt-5 border-0">
+        <div class="card-body pt-0">
+            <h2 class="card-title text-center my-4">
+                <?= _t('田园生活') ?>
+            </h2>
+            <form action="/user/login" method="POST">
+                <div class="mb-3">
+                    <input class="form-control form-control-lg" name="account" type="text" placeholder="<?= _t('用户名或邮箱') ?>" autofocus>
+                </div>
+                <div class="mb-3">
+                    <input class="form-control form-control-lg" name="password" type="text" placeholder="<?= _t('密码') ?>">
+                </div>
+                <div class="mb-3 d-grid">
+                    <button class="btn btn-primary btn-lg" type="submit"><?= _t('登录') ?></button>
+                </div>
+            </form>
+            <div class="d-flex justify-content-between">
+                <a class="link-secondary text-decoration-none" href="/"><?= _t('返回首页') ?></a>
+                <a class="link-secondary text-decoration-none" href="/admin/register.php"><?= _t('用户注册') ?></a>
             </div>
-            <div class="field">
-                <input class="input" name="password" type="password" placeholder="<?= _t('密码') ?>">
-            </div>
-            <div class="field">
-                <button class="button button-primary button-block" type="submit"><?= _t('登录') ?></button>
-            </div>
-        </form>
-        <div class="tools">
-            <a href="/"><?= _t('返回首页') ?></a>
-            <a href="/admin/register.php"><?= _t('用户注册') ?></a>
         </div>
     </div>
 </div>
--- admin/register.php
+++ admin/register.php
@@ -1,24 +1,28 @@
 <?php require __DIR__ . '/modules/header.php'; ?>
-<div class="login container">
-    <div class="board">
-        <h1 class="login-title"><?= _t('Idyll') ?></h1>
-        <form action="/user/register" method="POST">
-            <div class="field">
-                <input class="input" name="username" type="text" placeholder="<?= _t('用户名') ?>" autofocus>
+<div class="login">
+    <div class="board card mt-5 border-0">
+        <div class="card-body pt-0">
+            <h2 class="card-title text-center my-4">
+                <?= _t('田园生活') ?>
+            </h2>
+            <form action="/user/register" method="POST">
+                <div class="mb-3">
+                    <input class="form-control form-control-lg" name="username" type="text" placeholder="<?= _t('用户名') ?>" autofocus>
+                </div>
+                <div class="mb-3">
+                    <input class="form-control form-control-lg" name="email" type="email" placeholder="<?= _t('邮箱') ?>" autofocus>
+                </div>
+                <div class="mb-3">
+                    <input class="form-control form-control-lg" name="password" type="text" placeholder="<?= _t('密码') ?>">
+                </div>
+                <div class="mb-3 d-grid">
+                    <button class="btn btn-primary btn-lg" type="submit"><?= _t('注册') ?></button>
+                </div>
+            </form>
+            <div class="d-flex justify-content-between">
+                <a class="link-secondary text-decoration-none" href="/"><?= _t('返回首页') ?></a>
+                <a class="link-secondary text-decoration-none" href="/admin/login.php"><?= _t('用户登录') ?></a>
             </div>
-            <div class="field">
-                <input class="input" name="email" type="email" placeholder="<?= _t('邮箱') ?>">
-            </div>
-            <div class="field">
-                <input class="input" name="password" type="password" placeholder="<?= _t('密码') ?>">
-            </div>
-            <div class="field">
-                <button class="button button-primary button-block" type="submit"><?= _t('注册') ?></button>
-            </div>
-        </form>
-        <div class="tools">
-            <a href="/"><?= _t('返回首页') ?></a>
-            <a href="/admin/login.php"><?= _t('用户登录') ?></a>
         </div>
     </div>
 </div>
